3fe4cb495132b2a4db1263b1ab3e66ed73970ac9
Xiaoxuan Wang <xiaoxuanwang@Xiaoxuans-Laptop.local>
2025-04-10 09:09:41 -0700

    update command

--- train/run_east_sft.sh
+++ train/run_east_sft.sh
@@ -1,23 +1,25 @@
 
 # put your training data as TRAINING_DATA_PATH. Entropy exponent and coefficient are calculated based on the training data.
 EXPONENT=1
-COEFF=0.4
+COEFF=6.8
+GPU=0
+TRAINING_DATA_PATH=''
 
-ACCELERATE_LOG_LEVEL=info accelerate launch --config_file gpu.yaml dpo.py \
+CUDA_VISIBLE_DEVICES=$GPU ACCELERATE_LOG_LEVEL=info accelerate launch --config_file gpu.yaml sft.py \
     --dataset_name $TRAINING_DATA_PATH \
     --model_name_or_path meta-llama/Llama-3.2-1B-Instruct \
-    --learning_rate 2e-7 \
+    --learning_rate 2e-6 \
     --num_train_epochs 3 \
     --per_device_train_batch_size 2 \
     --gradient_accumulation_steps 8 \
     --gradient_checkpointing \
     --logging_steps 5 \
-    --output_dir ./model/Llama-3.2-1B-NEW-dpo-math-3epoch-lr2e-7-bs16-rejected_non_linear1 \
+    --output_dir ./model/Llama-3.2-1B-NEW-sft-gsm8k-3epoch-lr2e-6-bs16-entropy_non_linear1 \
     --no_remove_unused_columns \
     --report_to wandb \
     --save_strategy "no" \
     --warmup_ratio 0.1 \
-    --use_weighting  \
-    --use_rejected_weight_non_linear $EXPONENT \
-    --use_rejected_weight_non_linear_coeff $COEFF \
+    --use_weighting \
+    --use_entropy_non_linear $EXPONENT \
+    --use_entropy_non_linear_coeff $COEFF \
     --bf16 True
--- train/run_east_sft_accuracy.sh
+++ train/run_east_sft_accuracy.sh
@@ -1,26 +1,23 @@
 
 # put your training data as TRAINING_DATA_PATH. Entropy exponent and coefficient are calculated based on the training data.
 EXPONENT=1
-COEFF=6.8
-GPU=0
-TRAINING_DATA_PATH=''
+COEFF=0.6
 
-CUDA_VISIBLE_DEVICES=$GPU ACCELERATE_LOG_LEVEL=info accelerate launch --config_file gpu.yaml dpo.py \
+CUDA_VISIBLE_DEVICES=$GPU ACCELERATE_LOG_LEVEL=info accelerate launch --config_file gpu.yaml sft.py \
     --dataset_name $TRAINING_DATA_PATH \
     --model_name_or_path meta-llama/Llama-3.2-1B-Instruct \
-    --learning_rate 2e-7 \
+    --learning_rate 2e-6 \
     --num_train_epochs 3 \
     --per_device_train_batch_size 2 \
     --gradient_accumulation_steps 8 \
     --gradient_checkpointing \
     --logging_steps 5 \
-    --output_dir ./model/Llama-3.2-1B-NEW-dpo-gsm8k-3epoch-lr2e-7-bs16-beta0.01-entropy_non_linear1 \
-    --beta 0.01 \
+    --output_dir ./model/Llama-3.2-1B-NEW-sft-gsm8k-3epoch-lr2e-6-bs16-chosen_non_linear1 \
     --no_remove_unused_columns \
     --report_to wandb \
     --save_strategy "no" \
     --warmup_ratio 0.1 \
     --use_weighting \
-    --use_entropy_non_linear $EXPONENT \
-    --use_entropy_non_linear_coeff $COEFF \
+    --use_chosen_weight_non_linear $EXPONENT \
+    --use_chosen_weight_non_linear_coeff $COEFF \
     --bf16 True
